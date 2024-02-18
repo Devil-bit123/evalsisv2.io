2c27af73eb4dda0ac0db1017c82a26cdf80087b1
miguel000127@gmail.com <miguel000127@gmail.com>
2024-02-18 01:17:55 -0500

    # fix to send emails when a test its programed

--- app/Listeners/SendTestConfigurationNotification.php
+++ app/Listeners/SendTestConfigurationNotification.php
@@ -2,14 +2,13 @@
 
 namespace App\Listeners;
 
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
-use App\Events\TestConfigurationCreated;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use App\Mail\TestConfigurationCreated as TestConfigurationCreatedMail;
-use App\Models\Course;
 use App\Models\Exam;
+use App\Models\Course;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
+use App\Events\TestConfigurationCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class SendTestConfigurationNotification implements ShouldQueue
 {
@@ -33,12 +32,38 @@ class SendTestConfigurationNotification implements ShouldQueue
         $alumnos = $course->users()->wherePivot('role', 'alumno')->get();
 
         foreach ($alumnos as $alumno) {
-
             $email = $alumno->email;
-            Mail::to($email)->send(new TestConfigurationCreatedMail($testConfiguration,$question_bank,$course));
-
-            //Log::info('email: ' . json_encode($email));
+            $this->sendEmail($email, $testConfiguration, $question_bank, $course);
         }
+    }
 
+    private function sendEmail($to, $testConfiguration, $question_bank, $course)
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->post('https://api.sendinblue.com/v3/smtp/email', [
+                'json' => [
+                    'sender' => ['name' => 'EvalSis', 'email' => 'notifications@evalsis.com'],
+                    'to' => [['email' => $to]],
+                    'subject' => 'Notificación de Nuevos Examenes',
+                    'htmlContent' => view('emails.test_configuration_created', compact('testConfiguration', 'course'))->render()
+                ],
+                'headers' => [
+                    'api-key' => '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',
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode == 201) {
+                Log::info('Email sent successfully to: ' . $to);
+            } else {
+                Log::error('Failed to send email to: ' . $to . ', status code: ' . $statusCode);
+            }
+        } catch (\Exception $e) {
+            Log::error('Error sending email to: ' . $to . ', error: ' . $e->getMessage());
+        }
     }
 }
