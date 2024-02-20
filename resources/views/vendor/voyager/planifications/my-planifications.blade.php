d7391c58be965092bb0f3ec29fe3816192c75e52
miguel000127@gmail.com <miguel000127@gmail.com>
2024-02-20 11:20:52 -0500

    # add validations to my planifications

--- resources/views/vendor/voyager/planifications/my-planifications.blade.php
+++ resources/views/vendor/voyager/planifications/my-planifications.blade.php
@@ -1,6 +1,17 @@
 @extends('voyager::master')
 
 @section('content')
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
     <div class="card">
         <div class="card-body">
             <div class="card">
